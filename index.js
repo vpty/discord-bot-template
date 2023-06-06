703dd1329bce4e1f18307f3243ca296866d77520
vpty <81234274+vpty@users.noreply.github.com>
2023-06-06 16:46:24 +0200

    final release

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.DISCORD_TOKEN);
