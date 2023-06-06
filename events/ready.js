3866b5308c680a342dea38a914daaaa15c920e35
vpty <81234274+vpty@users.noreply.github.com>
2023-06-06 16:47:58 +0200

    Delete events directory

--- events/ready.js
+++
@@ -1,3 +0,0 @@
-client.on('ready', () => {
-    console.log(`Bot is ready! Logged in as ${client.user.tag}`);
-  });
--- events/welcome.js
+++
@@ -1,6 +0,0 @@
-client.on('guildMemberAdd', member => {
-    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome');
-    if (welcomeChannel) {
-      welcomeChannel.send(`Welcome, ${member.user.username}!`);
-    }
-  });
